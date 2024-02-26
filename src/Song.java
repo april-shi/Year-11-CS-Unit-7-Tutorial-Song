1997d7372b6ffe56aab113e9a79ad154883329f2
april <aprils2026@student.cis.edu.hk>
2024-02-26 14:33:31 +0800

    done!

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,61 @@
+import java.util.ArrayList;
+
 public class Song {
 
-    private String
+    private String title;
+    private String artist;
+
+    private ArrayList<String> people = new ArrayList<>();
+
+    public Song(String title, String artist) {
+
+        this.title = title;
+        this.artist = artist;
+
+    }
+
+    public String getArtist() {
+
+        return artist;
+
+    }
+
+    public String getTitle() {
+
+        return title;
+
+    }
+
+    public int howMany(ArrayList<String> names) {
+
+        int count = 0;
+
+        for (String name: names) {
+
+            name = name.toLowerCase();
+
+            boolean hasRepeat = false;
+
+            for (String newName: people) {
+
+                if (name.equals(newName)) {
+
+                    hasRepeat = true;
+
+                }
+
+            }
+
+            if (!hasRepeat) {
+
+                count++;
+                people.add(name);
+
+            }
+
+        }
+
+        return count;
+
+    }
 }
